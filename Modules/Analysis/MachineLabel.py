76aa1e96944b0fbd0c7a2f8e0e645b865c2360c2
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2019-06-12 13:06:53 -0400

    Test

--- Modules/Analysis/MachineLabel.py
+++ Modules/Analysis/MachineLabel.py
@@ -79,7 +79,7 @@ class MachineLabelCreator:
                 #subprocess.call(['rclone', 'copy', self.cloudClipsDirectory + projectID, self.localClipsDirectory + projectID], stderr = self.fnull)
                 videos = os.listdir(self.localClipsDirectory + projectID)
 
-                for videoID in videos:
+                for videoID in sorted(videos):
                     clips = os.listdir(self.localClipsDirectory + projectID + '/' + videoID)
                     print(clips)
                     for clip in clips:
@@ -97,7 +97,7 @@ class MachineLabelCreator:
 
                         if randint(0,4) == 4:
                             print(label + '/' + clip.replace('.mp4',''), file = g)
-                            outDirectory = self.localJpegDirectory + 'm/' + clip.remove('.mp4') + '/'
+                            outDirectory = self.localJpegDirectory + 'm/' + clip.replace('.mp4','') + '/'
                             os.makedirs(outDirectory) 
                             subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + projectID + '/' + videoID + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
                             # Add n_frames info
@@ -105,7 +105,7 @@ class MachineLabelCreator:
                                 print('120', file = h)
                         else:
                             print(label + '/' + clip.replace('.mp4',''), file = f)
-                            outDirectory = self.localJpegDirectory + label + '/' + clipå.remove('.mp4') + '/'
+                            outDirectory = self.localJpegDirectory + label + '/' + clip.replace('.mp4','') + '/'
                             os.makedirs(outDirectory) 
                             subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + projectID + '/' + videoID + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
                             with open(outDirectory + 'n_frames', 'w') as h:
