b3e6f6cb045a9245bb889b8c156fbd1d9112e885
Patrick McGrath <ptmcgrat@gmail.com>
2019-06-20 08:55:16 -0400

    Test

---
+++ mean.py
@@ -0,0 +1,32 @@
+def get_mean(norm_value=255, dataset='activitynet'):
+    assert dataset in ['activitynet', 'kinetics', 'cichlids']
+
+    if dataset == 'activitynet':
+        return [
+            114.7748 / norm_value, 107.7354 / norm_value, 99.4750 / norm_value
+        ]
+    elif dataset == 'kinetics':
+        # Kinetics (10 videos for each class)
+        return [
+            110.63666788 / norm_value, 103.16065604 / norm_value,
+            96.29023126 / norm_value
+        ]
+    elif dataset == 'cichlids':
+        return [
+            76.5387720376522 / norm_value,  92.73 / norm_value,
+            23.425955869324458 / norm_value
+        ]
+    Means: 7,92.72825193798462,,,Stds: 27.574904632126117,27.850116405228853,21.284397206143048
+
+
+def get_std(norm_value=255):
+    # Kinetics (10 videos for each class)
+
+    return [
+        27.57 / norm_value, 27.85 / norm_value, 21.28 / norm_value
+    ]
+    
+    #return [
+    #    38.7568578 / norm_value, 37.88248729 / norm_value,
+    #    40.02898126 / norm_value
+    #]
