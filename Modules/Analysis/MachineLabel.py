785de7bdc5c5441e41e2fcb3070d6982654be42c
Patrick McGrath <ptmcgrat@gmail.com>
2019-06-27 08:09:24 -0400

    Test

--- Modules/Analysis/MachineLabel.py
+++ Modules/Analysis/MachineLabel.py
@@ -249,7 +249,7 @@ class MachineLabelCreator:
                     shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
                     os.makedirs(outDirectory) 
                     #print(['ffmpeg', '-i', self.localClipsDirectory + projectID + '/' + videoID + '/' + clip, outDirectory + 'image_%05d.jpg'])
-                    #subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
+                    subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
 
                     frames = [x for x in os.listdir(outDirectory) if '.jpg' in x]
                     try:
