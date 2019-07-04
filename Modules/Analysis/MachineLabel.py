e9824d8b29fa97c91a8c0a5a7b78eeeca290d520
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2019-07-04 17:41:55 -0400

    Test

--- Modules/Analysis/MachineLabel.py
+++ Modules/Analysis/MachineLabel.py
@@ -291,10 +291,10 @@ class MachineLearningMaker:
                             
                     outDirectory = self.localClipsDirectory + label + '/' + clip.split('/')[-1].replace('.mp4','') + '/'
                     outDirectories.append(outDirectory)
-                    #shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
-                    #os.makedirs(outDirectory) 
+                    shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
+                    os.makedirs(outDirectory) 
                     #print(['ffmpeg', '-i', self.localClipsDirectory + projectID + '/' + videoID + '/' + clip, outDirectory + 'image_%05d.jpg'])
-                    #subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
+                    subprocess.call(['ffmpeg', '-i', self.localClipsDirectory + clip, outDirectory + 'image_%05d.jpg'], stderr = self.fnull)
 
                     frames = [x for x in os.listdir(outDirectory) if '.jpg' in x]
                     try:
