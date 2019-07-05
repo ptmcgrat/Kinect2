5bc627b6372c7b978bfaca4a766a83c980dc7c8a
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2019-07-04 22:16:52 -0400

    Test

--- Modules/Analysis/MachineLabel.py
+++ Modules/Analysis/MachineLabel.py
@@ -267,8 +267,8 @@ class MachineLearningMaker:
                         print(label + '/' + clip.split('/')[-1].replace('.mp4',''), file = g)
                         print(clip.split('/')[-1].replace('.mp4','') + ',Test,' + label + ',' + ':', file = h)
                             
-                    #outDirectory = self.localClipsDirectory + label + '/' + clip.split('/')[-1].replace('.mp4','') + '/'
-                    #outDirectories.append(outDirectory)
+                    outDirectory = self.localClipsDirectory + label + '/' + clip.split('/')[-1].replace('.mp4','') + '/'
+                    outDirectories.append(outDirectory)
                     #shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
                     #os.makedirs(outDirectory) 
                     #print(['ffmpeg', '-i', self.localClipsDirectory + projectID + '/' + videoID + '/' + clip, outDirectory + 'image_%05d.jpg'])
