4242bd31b2712e695ed4acb23dc75e1a9f5909c2
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2019-07-04 21:05:03 -0400

    Test

--- Modules/Analysis/MachineLabel.py
+++ Modules/Analysis/MachineLabel.py
@@ -153,7 +153,7 @@ class MachineLearningMaker:
 
         predictions = []
         for modelID in modelIDs:
-            localOutDir = self.localOutputDirectory + modelID + '/prediction/' + 
+            localOutDir = self.localOutputDirectory + modelID + '/prediction/'
             dt = pd.read_csv(self.localOutputDirectory + modelID + '/prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.classes, skiprows = [0], index_col = 0)
             softmax = dt.apply(scipy.special.softmax, axis = 1)
             prediction = pd.concat([softmax.idxmax(axis=1).rename(modelID + '_pred'), softmax.max(axis=1).rename(modelID + '_conf')], axis=1)
