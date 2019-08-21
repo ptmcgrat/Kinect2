85f1955ebe4cd05cddd93676124cc1b507ae5072
Patrick McGrath <ptmcgrat@gmail.com>
2019-08-21 12:35:36 -0400

    Test

--- fishCounter.py
+++ fishCounter.py
@@ -52,7 +52,8 @@ def prepareData():
                         else:
                             image_data['train']['CountingData/' + project + '/' + video + '/' + label + '/' + videofile] = int(label)
 
-    return CountingDataset(image_data['train'], data_transforms['train']), CountingDataset(image_data['val'], data_transforms['val']),
+    dataloaders = {x: torch.utils.data.DataLoader(CountingDataset(image_data[x]), batch_size=4, shuffle=True, num_workers=4, sampler=None) for x in ['train', 'val']}
+    return dataloaders
 
 def createModel(args):
     model_ft = models.resnet50(pretrained=True)
@@ -66,6 +67,77 @@ def createModel(args):
         model_ft.fc = nn.Linear(num_ftrs, 1)
     return model_ft
 
+def trainModel(dataloaders, model, criterion, optimizer, scheduler, num_epochs=100):
+
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for e in range(num_epochs):
+        print('Epoch {}/{}'.format(e, num_epochs - 1))
+        print('-' * 10)
+
+        #training/validation
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train() #set model to training mode
+            else:
+                model.eval() #set model to evaluation mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            #iterate over data
+            for inputs, labels in dataloaders[phase]:
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                #pdb.set_trace()
+
+                #zero parameter gradients
+                optimizer.zero_grad()
+
+                #forward pass
+                #track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    #output = outputs[:, -1] ###only use for L1 or MSELoss
+                    loss = criterion(outputs, labels) #should be labels.float() for L1 or MSELoss
+
+                    #backward pass / optimization only in training
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                #stats
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            #deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print() #empty line
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val accuracy: {:.4f}'.format(best_acc))
+
+    model.load_state_dict(best_model_wts)
+
+    return model
+
 parser = argparse.ArgumentParser()
 parser.add_argument('LossFunction', type = str, choices=['L1','L2','CE'], help = 'Loss functions L1, L2, or cross entropy')
 parser.add_argument('-u', '--unfreeze', action = 'store_true', help = 'Use if you want to fit all parameters')
@@ -77,7 +149,7 @@ args = parser.parse_args()
 subprocess.call(['rclone', 'copy', 'cichlidVideo:McGrath/Apps/CichlidPiData/__Counting/', 'CountingData'])
 
 # Prepare data to create training sets
-trainDataset, valDataset = prepareData()
+dataLoaders = prepareData()
 
 # Create model
 model = createModel(args)
@@ -105,3 +177,5 @@ optimizer_ft = torch.optim.Adam(model.parameters(), lr=0.0001)
 
 # Decay LR by a factor of 0.1 every 7 epoch_loss
 exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+model = trainModel(dataLoaders, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=100)
