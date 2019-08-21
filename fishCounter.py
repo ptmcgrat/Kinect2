d1d7fcc1f261da8a51cfa79a65aff45d6dce2756
Patrick McGrath <ptmcgrat@gmail.com>
2019-08-21 10:55:49 -0400

    Test

--- fishCounter.py
+++ fishCounter.py
@@ -1,6 +1,6 @@
 import argparse, subprocess, os, random
-from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 class CountingDataset(Dataset):
     """Face Landmarks dataset."""
@@ -11,33 +11,42 @@ class CountingDataset(Dataset):
         self.transforms = transforms
 
     def __getitem__(self, index):
-        """
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (image, target) where target is class_index of the target class.
-        """
-        
+       
+        with open(self.data_list[i], 'rb') as f:
+            img = Image.open(f).convert('RGB')
 
-
-        path = self.data[index]['video']
-        clip_name = path.rstrip().split('/')[-1]
-        frame_indices = self.data[index]['frame_indices']
-        if self.temporal_transform is not None:
-            frame_indices = self.temporal_transform(frame_indices)
-        clip = self.loader(path, frame_indices)
-        if self.spatial_transforms is not None:
-            self.spatial_transforms[self.annotationDict[clip_name]].randomize_parameters()
-            clip = [self.spatial_transforms[self.annotationDict[clip_name]](img) for img in clip]
-        clip = torch.stack(clip, 0).permute(1, 0, 2, 3)
-
-        target = self.data[index]
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return clip, target, path
+        return self.transforms(img), self.data_dict[self.data_list[index]]
 
     def __len__(self):
         return len(self.data_dict)
+
+def prepareData():
+
+    # Define transforms
+    data_transforms = {
+    'train': transforms.Compose([
+    transforms.RandomResizedCrop(size=224, scale=(0.9,1.0)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([100,100,100],[0.5,0.5,0.5])]),
+    'val': transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([100,100,100],[0.5,0.5,0.5])])
+    }
+
+    for project in [x for x in os.listdir('CountingData/') if x[0] != '.']:
+        for video in [x for x in os.listdir('CountingData/' + project) if x[0] != '.']:
+            for label in [x for x in os.listdir('CountingData/' + project + '/' + video) if x[0] != '.']:
+                if label != 'p':
+                    for videofile in [x for x in os.listdir('CountingData/' + project + '/' + video + '/' + label) if x[0] != '.']:
+                        if random.randint(0,4) == 0:
+                            image_data['val']['CountingData/' + project + '/' + video + '/' + label + '/' + videofile] = int(label)
+                        else:
+                            image_data['train']['CountingData/' + project + '/' + video + '/' + label + '/' + videofile] = int(label)
+
+    return CountingDataset(image_data['train'], data_transforms['train']), CountingDataset(image_data['val'], data_transforms['val']),
 
 parser = argparse.ArgumentParser()
 parser.add_argument('LossFunction', type = str, choices=['L1','L2','CE'], help = 'Loss functions L1, L2, or cross entropy')
@@ -49,17 +58,5 @@ args = parser.parse_args()
 # Download data
 subprocess.call(['rclone', 'copy', 'cichlidVideo:McGrath/Apps/CichlidPiData/__Counting/', 'CountingData'])
 
-image_data = {}
-image_data['train'] = {}
-image_data['val'] = {}
-
-for project in [x for x in os.listdir('CountingData/') if x[0] != '.']:
-    for video in [x for x in os.listdir('CountingData/' + project) if x[0] != '.']:
-        for label in [x for x in os.listdir('CountingData/' + project + '/' + video) if x[0] != '.']:
-            if label != 'p':
-                for videofile in [x for x in os.listdir('CountingData/' + project + '/' + video + '/' + label) if x[0] != '.']:
-                    if random.randint(0,4) == '0':
-                        image_data['val']['CountingData/' + project + '/' + video + '/' + label + '/' + videofile] = int(label)
-                    else:
-                        image_data['train']['CountingData/' + project + '/' + video + '/' + label + '/' + videofile] = int(label)
+trainDataset, valDataset = prepareData()
 
