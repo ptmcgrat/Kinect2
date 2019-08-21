92d804c98648814cc5dfb2b37375bc481d622c89
Patrick McGrath <ptmcgrat@gmail.com>
2019-08-21 13:23:13 -0400

    Test

--- fishCounter.py
+++ fishCounter.py
@@ -110,6 +110,7 @@ def trainModel(dataloaders, model, criterion, optimizer, scheduler, device, num_
                         _, preds = torch.max(outputs, 1)
                     else:
                         loss = criterion(outputs[:,-1], labels.float())
+                        pdb.set_trace()
                         preds = outputs.int()
 
                     if phase == 'train':
